e12506087ad7b5acc980fe1462be835854c44bfe
estebance <restebance@gmail.com>
2014-07-10 23:56:38 -0500

    Add rspec test to static_pages and finish

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe "StaticPages" do 
 
   describe "Home Page" do 
+
+    before { visit root_path}  
     
-    it "should have the title" do 
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Sample App | Home")
+    it "should have the title 'Sample App'" do
+      expect(page).to have_title(full_title('Home'))
     end 
 
 
     it "should have the content 'Sample App' " do 
-      visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end 	
   
@@ -19,34 +19,44 @@ describe "StaticPages" do
 
   describe "Help Page" do 
 
+    before { visit help_path}
+
     it "should have the title" do 
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Sample App | Help")
+      expect(page).to have_title(full_title('Help'))
     end 
 
     it "should have the content 'Help'" do 
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end 	
   end 
 
   describe "About Page" do 
 
+    before { visit about_path}
+
     it "should have the title" do 
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Sample App | About")
+      expect(page).to have_title(full_title('About'))
     end 
 
     it "should have the content 'About Us'" do 
-      visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end 	
     
   end 
 
+  describe "Contact page" do 
 
+    before { visit contact_path}
 
+    it "should have the title 'Contact'" do 
+      expect(page).to have_title(full_title('Contact'))
+    end
 
+    it "should have the content 'Contact'" do 
+      expect(page).to have_content("Contact")
+    end  
 
+  end 
+    
 end 	
 
