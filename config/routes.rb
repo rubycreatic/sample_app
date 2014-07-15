4f5b637d00e7fc730aa4d21a56fde3ba633b35e8
ViveLab <latharstudios@MacBook-Pro-de-Lathar.local>
2014-07-15 00:25:31 -0500

    Changed routes to get notation

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  match '/help'  , to: 'static_pages#help' , via: 'get'
+  get '/help'  , to: 'static_pages#help' 
   
-  match '/about' , to: 'static_pages#about' , via: 'get'
+  get '/about' , to: 'static_pages#about' 
   
-  match '/contact' , to: 'static_pages#contact' , via: 'get'
+  get '/contact' , to: 'static_pages#contact' 
 
 
 
