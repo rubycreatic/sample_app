ee8b44b26ae9b9bcfc96a9f797396fe929b12a06
estebance <restebance@gmail.com>
2014-07-22 15:53:34 -0500

    repair signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  #match '/signup' , to: "users#new", via: 'get'
+  match '/signup' , to: "users#new", via: 'get'
 
   match '/help'  , to: 'static_pages#help' , via: 'get'
   
