be1d026d8c62c224eeda6abcf4e556276b8172ed
estebance <restebance@gmail.com>
2014-07-23 23:32:37 -0500

    add sign up page to the app

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,5 +29,39 @@ describe "User pages" do
 
 	end 
 
+	describe "sign up proccess" do 
+
+		before { visit new_user_path }
+
+		let(:submit){"Create my account"}
+
+		describe "with invalid information" do 
+
+			it "should not create an user" do
+
+				expect {click_button submit}.not_to change(User, :count )
+
+			end 
+
+		end 
+
+		describe "with valid information" do 
+
+			before do 
+
+				fill_in "Name" , with: "Andres"
+				fill_in "Email" , with: "andres@example.com"
+				fill_in "Password" , with: "andres1"
+				fill_in "Confirmation" , with: "andres1"
+			end 
+
+			it "should create an user" do 
+				expect {click_button submit}.to change(User, :count ).by(1)
+			end 
+
+		end 
+
+
+	end 
 
 end 
